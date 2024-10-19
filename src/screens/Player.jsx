84c8d9eae9d78eae75421638dc7ed214abe7c210
Piero <pcapra@squiz.co.uk>
2024-10-19 16:08:50 +0100

    feat: Player header

--- src/screens/Player.jsx
+++ src/screens/Player.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+
+// Files
+import apiClient from "../spotify";
+
+// Styles
+import '../styles/player.css'
 
 export default function Player() {
+  const location = useLocation();
+  const { id } = location.state || {}; 
+  const [playlistData, setPlaylistData] = useState({})
+
+  useEffect(() => {
+      apiClient.get(`/playlists/${id}`).then(function(response) {
+        console.log(response)
+        setPlaylistData(response.data)
+    })
+  }, [])
+  
   return (
     <div className="screen-container">
-      Player
+      <div className="player-container">
+        <div className="player-header">
+          <div className="playlist-img">
+            <img src={playlistData?.images?.[0]?.url} alt="" />
+          </div>
+          <div className="playlist-details">
+            <h1>{playlistData?.name}</h1>
+            <h6>{playlistData?.description}</h6>
+            <div className="playlist-info tag">
+              <p>{playlistData?.tracks?.items?.length} Songs - {playlistData?.followers?.total} Followers</p>
+            </div>
+          </div>
+        </div>
+        <div className="player-content">
+          <ul>
+            {playlistData?.tracks?.items?.map((track)=>{
+              return <p key={track.track.id} >{track.track.name}</p>
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
