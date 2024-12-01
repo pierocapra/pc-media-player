1782eb2e41075cd78787f810f3210c7c10c867ed
Piero <pcapra@squiz.co.uk>
2024-12-01 14:06:44 +0000

    fix: Fix null playlist

--- src/screens/Library.jsx
+++ src/screens/Library.jsx
@@ -33,26 +33,30 @@ export default function Library({setPlaylist}) {
     return (
         <div className="screen-container">
           <div className="library-body">
-            {playlists?.map((playlist) => (
-              <div
-                className="playlist-card"
-                key={playlist.id}
-                onClick={() => playPlaylist(playlist.id)}
-              >
-                <img
-                  src={playlist?.images?.[0]?.url}
-                  className="playlist-image"
-                  alt="Playlist-Art"
-                />
-                <p className="playlist-title">{playlist.name}</p>
-                <p className="playlist-subtitle">{playlist.tracks.total} Songs</p>
-                <div className="playlist-fade">
-                  <IconContext.Provider value={{ size: "50px", color: "#E99D72" }}>
-                    <AiFillPlayCircle />
-                  </IconContext.Provider>
-                </div>
-              </div>
-            ))}
+            {playlists?.map((playlist) => {
+              if (!playlist) return null; // Skip null or undefined playlists
+              return ( 
+                    <div
+                      className="playlist-card"
+                      key={playlist?.id}
+                      onClick={() => playPlaylist(playlist?.id)}
+                    >
+                      <img
+                        src={playlist?.images?.[0]?.url}
+                        className="playlist-image"
+                        alt="Playlist-Art"
+                      />
+                      <p className="playlist-title">{playlist?.name}</p>
+                      <p className="playlist-subtitle">{playlist?.tracks?.total} Songs</p>
+                      <div className="playlist-fade">
+                        <IconContext.Provider value={{ size: "50px", color: "#E99D72" }}>
+                          <AiFillPlayCircle />
+                        </IconContext.Provider>
+                      </div>
+                    </div>
+                  )
+                }
+              )}
           </div>
         </div>
       );
